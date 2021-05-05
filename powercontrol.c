609bf6b62ef66cd5f536366a3af17a1af43f6a7e
Torenjk <33992305+Torenjk@users.noreply.github.com>
2021-05-05 14:40:08 +0200

    SMC_MODE selector changed to shift operation

--- powercontrol.c
+++ powercontrol.c
@@ -1,5 +1,8 @@
-//Tom Kamprath
-//AVR AT328P Sleep mode
+/*
+ Author: Tom Kamprath
+ Description: AVR AT328* Power & Sleep Mode Controller
+ */
+
 #include <stdio.h>
 
 static uint8_t* SCR = 0x61; //SC Register
@@ -13,22 +16,26 @@ uint8_t* INTR_PORT = 0x0;
 bool IntrAsOutp = 0; //Interrupt Pin can be Output too
 
 void SMC_SET(uint8_t SMC_MODE, bool BODS_DISABLE){
-    if(SMC_MODE > 5) break; //Invalid Bits set
+    if(SMC_MODE > 5) foo(); //Still need to implement error handling
 
     if (BODS_DISABLE){
         *MCU_BOD = (1 << 7); 
         *MCU_BOD = (1 << 6); 
-        *MCU_BOD = (0 << 8); //Not sure if this shifts right
+        *MCU_BOD &= ~(1 << 8); //Shift zero
     } 
 
     //Todo: Change this to shift operation!
     switch (SMC_MODE){
         case 0: *SCR = 0; //IDLE
-        case 1: *SCR = 2; //ADC
-        case 2: *SCR = 4; //Power-down
-        case 3: *SCR = 6; //Power-save
-        case 4: *SCR = 12; //Standby
-        case 5: *SCR = 14; //ext. Standby
+        case 1: *SCR = (1 << 1); //ADC
+        case 2: *SCR = (1 << 2); //Power-down
+        case 3: *SCR = (1 << 1); //*SCR = 6
+                *SCR = (1 << 2); 
+        case 4: *SCR = (1 << 2); //*SCR = 12
+                *SCR = (1 << 3);
+        case 5: *SCR = (1 << 1);
+                *SCR = (1 << 2);
+                *SCR = (1 << 3); //*SCR = 14 ext. Standby
     }
 
 }
